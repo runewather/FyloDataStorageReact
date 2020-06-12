876e7371d6fd562073b1b209d794254588367cf7
Victo Principe <victopessoa46@gmail.com>
2020-06-12 18:19:23 -0300

    added favicon

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Favicon from 'react-favicon';
 import App from './App';
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url="public/favicon-32x32.png" />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
